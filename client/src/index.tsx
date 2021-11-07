ec6e616a4640bc70b4f4f1164f953ef821995613
banti377 <patelbanti27@yahoo.in>
2021-11-07 13:34:40 +0530

    apollo client implemented

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,19 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import App from './App';
+
+const client = new ApolloClient({
+  uri: 'http://192.168.29.124:5000',
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
